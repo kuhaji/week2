e1dea4b8e58d6c1d8297f1b71a48e35e86814638
Juha <juhavali@metropolia.fi>
2021-01-06 15:18:32 +0200

    update

--- app.js
+++ app.js
@@ -8,15 +8,11 @@ const catRoute = require('./routes/catRoute');
 const userRoute = require('./routes/userRoute');
 
 app.use(cors());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
+
 app.use('/cat', catRoute);
 app.use('/user', userRoute);
-
-app.listen(port, () =>
-    console.log(`Example app listening on port ${port}!`));
-=======
-const express = require('express');
-const app = express();
-const port = 3000;
 
 app.get('/cat', (req, res) => {
   res.send('From this endpoint you can get cats.')
