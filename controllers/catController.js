15fe975d288b29ff8d778c378d4a3854b70b86fb
Juha <juhavali@metropolia.fi>
2021-01-06 16:43:56 +0200

    update

--- controllers/catController.js
+++ controllers/catController.js
@@ -8,21 +8,49 @@ const cat_list_get = async (req, res) => {
     res.json(cats);
 };
 
-const cat_get = (req, res) => {
+const cat_get = async (req, res) => {
     console.log('cat id parameter', req.params);
-    const cat = cats.filter(cat => cat.id === req.params.id).pop();
+    const cat = await catModel.getCat(req.params.id);
     res.json(cat);
 };
 
-const cat_post = (req, res) => {
-    console.log('cat_post', req.body);
-    res.send('With this endpoint you can add cats');
+const cat_post = async (req, res) => {
+    console.log('cat_post', req.body, req.file);
+    const inCat = {
+        name: req.body.name,
+        age: req.body.age,
+        weight: req.body.weight,
+        owner: req.body.owner,
+        filename: req.file.filename,
+    };
+    try {
+        const cat = await catModel.insertCat(inCat);
+        console.log('inserted', cat);
+        res.send(`added cat: ${cat.insertId}`);
+    } catch (e) {
+        console.error('problem with cat_post in catController', e);
+        res.status(500).send(`database insert error: ${e.message}`);
+    }
+};
+
+const cat_put = async (req, res) => {
+    console.log('cat_put', req.body);
+    const upCat = await catModel.updateCat(req.body);
+    console.log('cat_put result from db', upCat);
+    res.status(204).send();
+};
+
+const cat_delete = async (req, res) => {
+    console.log('cat_put', req.parms);
+    const delCat = await catModel.deleteCat(req.params.id);
+    console.log('cat_delete result from db', delCat);
+    res.json({ deleted: 'OK' });
 };
 
 module.exports = {
     cat_list_get,
     cat_get,
     cat_post,
+    cat_put,
+    cat_delete,
 };
-
-
